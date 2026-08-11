0a3b4663c0223d4409ac5b528e082a49a6666ea8
Marin <marin.procureur@gmail.com>
2026-08-11 22:35:17 +0200

    feat(plan): add plan catalog and limit predicate

---
+++ packages/modules/src/plan/domain/index.ts
@@ -0,0 +1,4 @@
+export { planCatalog } from "./catalog.js";
+export { exceedsLimit } from "./limit.js";
+export { planNameSchema } from "./plan.js";
+export type { LimitName, PlanEntitlements, PlanFeatures, PlanLimits, PlanName } from "./plan.js";
